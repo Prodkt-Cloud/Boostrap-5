ab4886a22bf40cc73438ccc7eb4cb582d084e7dc
Bryan Funk <technology@prodkt.cloud>
2022-05-27 09:57:16 -0600

    Init - Bs5beta+Webpack Config

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+      },
+
+      devServer: {
+        static: {
+          directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+      },
+
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [{
+            // inject CSS to page
+            loader: 'style-loader'
+          }, {
+            // translates CSS into CommonJS modules
+            loader: 'css-loader'
+          }, {
+            // Run postcss actions
+            loader: 'postcss-loader',
+            options: {
+              // `postcssOptions` is needed for postcss 8.x;
+              // if you use postcss 7.x skip the key
+              postcssOptions: {
+                // postcss plugins, can be exported to postcss.config.js
+                plugins: () => {
+                  [
+                    require('autoprefixer')
+                  ];
+                }
+              }
+            }
+          }, {
+            // compiles Sass to CSS
+            loader: 'sass-loader'
+          }]
+        },
+      ],
+    },
+  };
